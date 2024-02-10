25652585d3832507dfd1f01bf613c50ab0ebd4d8
Tarique Anowar <anowartarique5@gmail.com>
2024-02-11 03:27:59 +0530

    Second  commit

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -57,6 +57,7 @@ export async function POST(req: Request) {
   const eventType = evt.type;
 
   if (eventType === "user.created") {
+    /*
     const { id, email_addresses, image_url, first_name, last_name, username } =
       evt.data;
 
@@ -78,7 +79,8 @@ export async function POST(req: Request) {
         },
       });
     }
-    return NextResponse.json({ message: "OK", user: newUser });
+    */
+    return NextResponse.json({ message: "OK" });
   }
 
   return new Response("", { status: 200 });
