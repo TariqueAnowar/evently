61b691e2f43f2e7b0894055be357ff08bf8ce217
Tarique Anowar <anowartarique5@gmail.com>
2024-03-12 02:32:17 +0530

    Stripe webhook added. Api route error fixed. Renamed path to webhook from webhooks. 3nd time fix.

--- app/api/webhook/stripe/route.ts
+++ app/api/webhook/stripe/route.ts
@@ -1,37 +1,39 @@
-import { NextResponse } from "next/server";
 import stripe from "stripe";
+import { NextResponse } from "next/server";
+//import { createOrder } from "@/lib/actions/order.actions";
 
-export async function POST(req: Request) {
-  const body = await req.text();
-  const sig = req.headers.get("stripe-signature") as string;
+export async function POST(request: Request) {
+  const body = await request.text();
+
+  const sig = request.headers.get("stripe-signature") as string;
   const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET!;
 
+  let event;
+
   try {
-    const event = stripe.webhooks.constructEvent(body, sig, endpointSecret);
-
-    let newOrder;
-
-    switch (event.type) {
-      case "checkout.session.completed":
-        const { id, amount_total, metadata } = event.data.object;
-
-        newOrder = {
-          stripeId: id,
-          eventId: metadata?.eventId,
-          buyerId: metadata?.buyerId,
-          amount_total: amount_total ? (amount_total / 100).toString() : "0",
-          createdAt: new Date(),
-        };
-
-        break;
-
-      default:
-        console.log(`Unhandled event type ${event.type}`);
-    }
-
-    return NextResponse.json({ message: "OK", order: newOrder });
-  } catch (err: any) {
-    console.error(`Webhook Error: ${err.message}`);
+    event = stripe.webhooks.constructEvent(body, sig, endpointSecret);
+  } catch (err) {
+    return NextResponse.json({ message: "Webhook error", error: err });
   }
-  return new Response("okay", { status: 200 });
+
+  // Get the ID and type
+  const eventType = event.type;
+
+  // CREATE
+  if (eventType === "checkout.session.completed") {
+    const { id, amount_total, metadata } = event.data.object;
+
+    const order = {
+      stripeId: id,
+      eventId: metadata?.eventId || "",
+      buyerId: metadata?.buyerId || "",
+      totalAmount: amount_total ? (amount_total / 100).toString() : "0",
+      createdAt: new Date(),
+    };
+
+    //const newOrder = await createOrder(order);
+    return NextResponse.json({ message: "OK", order: order });
+  }
+
+  return new Response("", { status: 200 });
 }
