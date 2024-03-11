2995db4a216b425b5039e094990e6283f15279c0
Tarique Anowar <anowartarique5@gmail.com>
2024-03-12 02:17:07 +0530

    Stripe webhook added. Api route error fixed. Renamed path to webhook from webhooks. 2nd time fix.

--- app/api/webhook/stripe/route.ts
+++ app/api/webhook/stripe/route.ts
@@ -1,49 +1,37 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse } from "next/server";
 import stripe from "stripe";
 
-const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET!;
+export async function POST(req: Request) {
+  const body = await req.text();
+  const sig = req.headers.get("stripe-signature") as string;
+  const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET!;
 
-export default async function webhookHandler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  if (req.method === "POST") {
-    const sig = req.headers["stripe-signature"] as string;
+  try {
+    const event = stripe.webhooks.constructEvent(body, sig, endpointSecret);
 
-    try {
-      const event = stripe.webhooks.constructEvent(
-        req.body,
-        sig,
-        endpointSecret
-      );
+    let newOrder;
 
-      let newOrder;
-      // Handle the event
-      switch (event.type) {
-        case "checkout.session.completed":
-          const { id, amount_total, metadata } = event.data.object;
+    switch (event.type) {
+      case "checkout.session.completed":
+        const { id, amount_total, metadata } = event.data.object;
 
-          newOrder = {
-            stripeId: id,
-            eventId: metadata?.eventId,
-            buyerId: metadata?.buyerId,
-            amount_total: amount_total ? (amount_total / 100).toString() : "0",
-            createdAt: new Date(),
-          };
+        newOrder = {
+          stripeId: id,
+          eventId: metadata?.eventId,
+          buyerId: metadata?.buyerId,
+          amount_total: amount_total ? (amount_total / 100).toString() : "0",
+          createdAt: new Date(),
+        };
 
-          break;
-        // ... handle other event types
-        default:
-          console.log(`Unhandled event type ${event.type}`);
-      }
+        break;
 
-      res.status(200).json({ message: "OK", order: newOrder });
-    } catch (err: any) {
-      console.error(`Webhook Error: ${err.message}`);
-      res.status(400).json({ error: `Webhook Error: ${err.message}` });
+      default:
+        console.log(`Unhandled event type ${event.type}`);
     }
-  } else {
-    res.setHeader("Allow", "POST");
-    res.status(405).end("Method Not Allowed");
+
+    return NextResponse.json({ message: "OK", order: newOrder });
+  } catch (err: any) {
+    console.error(`Webhook Error: ${err.message}`);
+    return new Response("Webhook Error", { status: 200 });
   }
 }
