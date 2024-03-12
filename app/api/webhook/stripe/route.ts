110733a7339dc506ad82c2557d7eac7c62a58386
Tarique Anowar <anowartarique5@gmail.com>
2024-03-13 03:30:27 +0530

    new Order added to MongoDB.

--- app/api/webhook/stripe/route.ts
+++ app/api/webhook/stripe/route.ts
@@ -1,43 +1,39 @@
 import stripe from "stripe";
 import { NextResponse } from "next/server";
+import { createOrder } from "@/lib/actions/order.actions";
 
 export async function POST(request: Request) {
   const body = await request.text();
+
   const sig = request.headers.get("stripe-signature") as string;
   const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET!;
 
+  let event;
+
   try {
-    const event = stripe.webhooks.constructEvent(body, sig, endpointSecret);
-
-    if (event.type === "checkout.session.completed") {
-      const { id, amount_total, metadata } = event.data.object;
-
-      const order = {
-        stripeId: id,
-        eventId: metadata?.eventId || "",
-        buyerId: metadata?.buyerId || "",
-        totalAmount: amount_total ? (amount_total / 100).toString() : "0",
-        createdAt: new Date(),
-      };
-
-      const jsonResponse = JSON.stringify({ message: "OK", order });
-
-      console.log("JSON Response:", jsonResponse);
-
-      return new Response(jsonResponse, {
-        status: 200,
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-    }
+    event = stripe.webhooks.constructEvent(body, sig, endpointSecret);
   } catch (err) {
-    console.error("Webhook error:", err);
-
     return NextResponse.json({ message: "Webhook error", error: err });
   }
 
-  // For other events or errors, return a generic response
-  console.log("Generic Response Executed");
+  // Get the ID and type
+  const eventType = event.type;
+
+  // CREATE
+  if (eventType === "checkout.session.completed") {
+    const { id, amount_total, metadata } = event.data.object;
+
+    const order = {
+      stripeId: id,
+      eventId: metadata?.eventId || "",
+      buyerId: metadata?.buyerId || "",
+      totalAmount: amount_total ? (amount_total / 100).toString() : "0",
+      createdAt: new Date(),
+    };
+
+    const newOrder = await createOrder(order);
+    return NextResponse.json({ message: "OK", order: newOrder });
+  }
+
   return new Response("Response Executed", { status: 200 });
 }
